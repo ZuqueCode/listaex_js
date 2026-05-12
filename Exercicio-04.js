5ab767d45c9cf07341212d9c829dbf62a124d7f1
ZuqueCode <nicolas_z_araujo@estudante.sesisenai.org.br>
2026-05-12 18:31:41 -0300

    feat -feito exercico 4 de ordenaçao de valores

---
+++ Exercicio-04.js
@@ -0,0 +1,13 @@
+function ordenarValores() {
+    let num1 = parseInt(prompt("Digite o primeiro número:"));
+    let num2 = parseInt(prompt("Digite o segundo número:"));
+    let num3 = parseInt(prompt("Digite o terceiro número:"));
+    let num4 = parseInt(prompt("Digite o quarto número:"));
+
+    let numeros = [num1, num2, num3, num4];
+    numeros.sort((a, b) => b - a);
+
+    console.log("Os números em ordem decrescente são: " + numeros);
+}
+
+ordenarValores();
