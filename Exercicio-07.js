64f9769636519915aea3bac3af4d71365fe06ad6
ZuqueCode <nicolas_z_araujo@estudante.sesisenai.org.br>
2026-05-12 18:41:14 -0300

    FEAT - feito exercicio numero 7 , sistema de preços do picolé

---
+++ Exercicio-07.js
@@ -0,0 +1,43 @@
+function sistemaDePrecos() {
+    let preco = 0;
+    let sabor = prompt("Escolha um sabor (a: Chocolate, b: Morango, c: Creme, d: Manga, e: Melancia, f: Vanilla Ice, g: Céu Azul, h: Brownie, i: Hawaiano):");
+
+    switch (sabor) {
+        case 'a':
+            preco = 1.50;
+            break;
+        case 'b':
+            preco = 2.50;
+            break;
+        case 'c':
+            preco = 2.50;
+            break;
+        case 'd':
+            preco = 3.20;
+            break;
+        case 'e':
+            preco = 3.40;
+            break;
+        case 'f':
+            preco = 3.00;
+            break;
+        case 'g':
+            preco = 3.60;
+            break;
+        case 'h':
+            preco = 4.00;
+            break;
+        case 'i':
+            preco = 5.00;
+            break;
+        default:
+            console.log("Sabor inválido");
+            break;
+    }
+
+    if (preco > 0) {
+        console.log(`Preço do picolé escolhido: R$ ${preco}`);
+    }
+}
+
+sistemaDePrecos();
